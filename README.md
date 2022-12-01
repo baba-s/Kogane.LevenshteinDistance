7dcf146db1d1cc75b6f948d1ac57074366b46b8c
baba-s <shotababa1206@gmail.com>
2022-12-01 21:12:33 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Kogane.LevenshteinDistance
+# Kogane Levenshtein Distance
+
+文字列の配列やリストから最も類似している文字列を検索できる拡張子メソッド
+
+## 使用例
+
+```csharp
+using Kogane;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        var texts = new[]
+        {
+            "Awake",
+            "Start",
+            "Update",
+            "FixedUpdate",
+            "LateUpdate",
+        };
+
+        // Update
+        Debug.Log( texts.GetMostSimilarString( "Updata" ) );
+    }
+}
+```
---
+++ Runtime/LevenshteinDistance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kogane
+{
+    public static class LevenshteinDistance
+    {
+        public static string GetMostSimilarString( this IEnumerable<string> self, string source )
+        {
+            return self
+                    .OrderByDescending( x => CalculateSimilarity( source, x ) )
+                    .FirstOrDefault()
+                ;
+        }
+
+        // https://social.technet.microsoft.com/wiki/contents/articles/26805.c-calculating-percentage-similarity-of-2-strings.aspx
+        public static int ComputeLevenshteinDistance( string source, string target )
+        {
+            if ( ( source == null ) || ( target == null ) ) return 0;
+            if ( ( source.Length == 0 ) || ( target.Length == 0 ) ) return 0;
+            if ( source == target ) return source.Length;
+
+            var sourceWordCount = source.Length;
+            var targetWordCount = target.Length;
+
+            if ( sourceWordCount == 0 ) return targetWordCount;
+            if ( targetWordCount == 0 ) return sourceWordCount;
+
+            var distance = new int[ sourceWordCount + 1, targetWordCount + 1 ];
+
+            for ( var i = 0; i <= sourceWordCount; distance[ i, 0 ] = i++ )
+            {
+            }
+
+            for ( var j = 0; j <= targetWordCount; distance[ 0, j ] = j++ )
+            {
+            }
+
+            for ( var i = 1; i <= sourceWordCount; i++ )
+            {
+                for ( var j = 1; j <= targetWordCount; j++ )
+                {
+                    var cost = ( target[ j - 1 ] == source[ i - 1 ] ) ? 0 : 1;
+                    distance[ i, j ] = Math.Min( Math.Min( distance[ i - 1, j ] + 1, distance[ i, j - 1 ] + 1 ), distance[ i - 1, j - 1 ] + cost );
+                }
+            }
+
+            return distance[ sourceWordCount, targetWordCount ];
+        }
+
+        // https://social.technet.microsoft.com/wiki/contents/articles/26805.c-calculating-percentage-similarity-of-2-strings.aspx
+        public static double CalculateSimilarity( string source, string target )
+        {
+            if ( ( source == null ) || ( target == null ) ) return 0.0;
+            if ( ( source.Length == 0 ) || ( target.Length == 0 ) ) return 0.0;
+            if ( source == target ) return 1.0;
+
+            var stepsToSame = ComputeLevenshteinDistance( source, target );
+            return ( 1.0 - ( stepsToSame / ( double )Math.Max( source.Length, target.Length ) ) );
+        }
+    }
+}
